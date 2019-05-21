6024fb13b2354c046d650c35e57ab5d29d998498
Aniruddha Maru <aniruddhamaru@gmail.com>
2019-05-21 12:34:27 -0700

    Allow disabling firehose

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ resource "google_pubsub_topic" "topic" {
 data "google_client_config" "current" {}
 
 resource "google_dataflow_job" "firehose" {
+  count = "${var.enable_firehose_all_messages ? 1 : 0}"
+
   name              = "${google_pubsub_topic.topic.name}-firehose"
   temp_gcs_location = "${var.dataflow_tmp_gcs_location}"
   template_gcs_path = "${var.dataflow_template_gcs_path}"
